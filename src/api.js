5ae44092d8d6edc6331048c9a5e665fcc25714c2
Muhammad Zain Yousuf <my06200@st.habib.edu.pk>
2022-07-18 16:31:39 +0500

    Weather App Completed

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '076a9fb1a3mshd1591d094df6c4cp105b02jsn21b07e21e048',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "5941b0eed9063d79e91f8d53d576444e";
